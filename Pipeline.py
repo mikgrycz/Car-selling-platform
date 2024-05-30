cecc31c0e9b7689b4e39a6199c7676c535583732
mikgrycz <mikolaj.grycz@student.uj.edu.pl>
2024-05-30 15:42:52 +0200

    Car price prediction

--- Pipeline.py
+++ Pipeline.py
@@ -17,8 +17,17 @@ from sklearn.metrics import mean_squared_error
 data = pd.read_csv("car_listings.csv")
 print(data.columns)
 print(data.shape)
-# remove duplicate records by ID
+data = data[data['engine_volume'] != 'Elektryczny']
+data = data[data['fuel_type'] != 'Elektryczny']
+data = data.dropna(thresh=13)
 data = data.drop_duplicates(subset='ID')
+nan_rows = data[data.isna().any(axis=1)]
+data = data.drop('fuel_type', axis=1)
+
+print(f"nan rows={nan_rows}")
+
+# remove duplicate records by ID
+
 # drop empty rows
 le = LabelEncoder()
 for col in ['brand', 'model', 'transmission', 'bodytype', 'colour']:
@@ -31,18 +40,31 @@ print(data['currency'].unique())
 data['price'] = data['price'].str.replace(' ', '')
 data['ID'] = data['ID'].str.replace('ID: ', '')
 data = data.drop('ID', axis=1)
-data = data.drop('fuel_type', axis=1)
-data = data.drop('transmission', axis=1)
-data = data.drop('colour', axis=1)
 data['mileage'] = data['mileage'].str.replace(' ', '')
 data['mileage'] = data['mileage'].str.replace('km', '')
 data['engine_volume'] = data['engine_volume'].str.replace(' ', '')
 data['engine_volume'] = data['engine_volume'].str.replace('cm3', '')
 data['power'] = data['power'].str.replace(' ', '')
 data['power'] = data['power'].str.replace('KM', '')
+cols = ['price', 'year', 'mileage', 'engine_volume', 'power']
+for col in cols:
+    # Convert column to string
+    data[col] = data[col].astype(str)
+    # Remove non-numeric characters
+    data[col] = data[col].str.replace(r'\D', '')
+    # Convert column to numeric, non-numeric become NaN
+    data[col] = pd.to_numeric(data[col], errors='coerce')
+
+# Drop rows with NaN values in the specified columns
+data = data.dropna(subset=cols)
+
+
 data['power'] = pd.to_numeric(data['power'], errors='coerce')
 data = data.dropna(subset=['power'])
 data['price'] = pd.to_numeric(data['price'], errors='coerce')
+
+
+
 
 conversion_rates = {'EUR': 4.5, 'USD': 3.8, 'GBP': 5.2}
 for currency, rate in conversion_rates.items():
@@ -80,21 +102,25 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_
 print(f'x_train shape: {x_train.shape}, x_test shape: {x_test.shape}, y_train shape: {y_train.shape}, y_test shape: {y_test.shape}')
 
 model_1 = linear_model.LinearRegression()
+model_1.fit(x_train, y_train)
+pred_train = model_1.predict(x_train)
+pred_test = model_1.predict(x_test)
+print(f'Linear regression RMSE on train: {mean_squared_error(y_train, pred_train, squared=False)}, RMSE on test: {mean_squared_error(y_test, pred_test, squared=False)}')
 
-model_2 = keras.Sequential([Dense(30, input_shape=[7], activation='relu'),
-                           Dense(60, activation='relu'),
+
+model_2 = keras.Sequential([Dense(30, input_shape=[9], activation='softmax'),
+                           Dense(50, activation='relu'),
                            Dense(40, activation='relu'),
                            Dense(30, activation='relu'),
-                           Dense(25, activation='relu'),
                            Dense(10, activation='relu'),
                            Dense(1)])
 
-model_3 = keras.Sequential([Dense(20, input_shape=[7], activation='relu'),
-                           Dense(40, activation='relu'),
+model_3 = keras.Sequential([Dense(20, input_shape=[9], activation='relu'),
+                           Dense(40, activation='sigmoid'),
                            Dense(10, activation='relu'),
                            Dense(1)])
 
-model_4 = keras.Sequential([Dense(7, input_shape=[7], activation='relu'),
+model_4 = keras.Sequential([Dense(7, input_shape=[9], activation='relu'),
                             Dense(6, activation='softmax'),
                             Dense(5, activation='relu'),
                             Dense(1)])
@@ -105,7 +131,7 @@ print("training models: ")
 for i, model in enumerate(models):
     model.compile(optimizer='adam', loss='mean_squared_error',metrics=['mean_absolute_error'])
 
-    model.fit(x_train, y_train, epochs=100, batch_size=20)
+    model.fit(x_train, y_train, epochs=1000, batch_size=300)
     pred_train = model.predict(x_train)
     pred_test = model.predict(x_test)
     #results.append(f'{model} RMSE on train: {mean_squared_error(y_test, pred_test, squared=False)},{model} RMSE on test: {mean_squared_error(y_train, pred_train, squared=False)}, accuracy {model.evaluate(x_test, y_test)}')
